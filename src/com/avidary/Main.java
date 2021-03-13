3d3abddbfcb59609b7e50d5a3a275a21d3562f6d
avidar <avidary@gmail.com>
2021-03-13 21:22:31 +0200

    Adding print statement

--- src/com/avidary/Main.java
+++ src/com/avidary/Main.java
@@ -3,6 +3,7 @@ package com.avidary;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(" This my fist push Via IDE");
+
     }
 }
