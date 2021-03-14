67294f32be2f2cfc97fdbff988e7b0c2730ae98b
avidar <avidary@gmail.com>
2021-03-14 09:14:42 +0200

    add remark on line 6

--- src/com/avidary/Main.java
+++ src/com/avidary/Main.java
@@ -3,7 +3,7 @@ package com.avidary;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(" This my first push Via IDE");
+        //System.out.println(" This my first push Via IDE");
         System.out.println("This is a new git branch");
 
     }
